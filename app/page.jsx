9be48ee6d3858bf88db92e1998c3fbb5fc64dd62
Anezz12 <harsenaargrtya1@gmail.com>
2024-08-10 00:26:19 +0700

    update Hero Section

--- app/page.jsx
+++ app/page.jsx
@@ -1,17 +1,11 @@
 import Link from "next/link";
-
+import Hero from "@/components/Hero";
+import InfoBoxes from "@/components/InfoBoxes";
 export default function Homepage() {
   return (
     <>
-      <div>
-        <h1 className="text-6xl text-center font-bold">Hello, Next.js!</h1>
-        <p className="text-center mt-10">This is a sample page.</p>
-      </div>
-      <div>
-        <Link href="/properties">
-          <h1 className="text-center underline mt-20">GO TO PROPERTIES</h1>
-        </Link>
-      </div>
+      <Hero />
+      <InfoBoxes />
     </>
   );
 }
