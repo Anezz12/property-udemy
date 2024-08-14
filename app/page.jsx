0014b6f3deab8dfb9e39c76ce5749ca13a389097
Anezz12 <harsenaargrtya1@gmail.com>
2024-08-14 16:14:20 +0700

    add Home Properties

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,12 @@
-import Link from "next/link";
 import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
+import HomePropertiesPage from "@/components/HomeProperties";
 export default function Homepage() {
   return (
     <>
       <Hero />
       <InfoBoxes />
+      <HomePropertiesPage />
     </>
   );
 }
