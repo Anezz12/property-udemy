7dc89fd69c23acc46b646a6ce29356e5486a2449
Anezz12 <harsenaargrtya1@gmail.com>
2024-08-29 22:05:07 +0700

    chore: Update dependencies and add PropertyDetails component

--- components/PropertyDetails.jsx
+++ components/PropertyDetails.jsx
@@ -1,3 +1,4 @@
+import Property from "@/models/Property";
 import {
   FaTimes,
   FaBed,
@@ -6,6 +7,7 @@ import {
   FaCheck,
   FaMapMarker,
 } from "react-icons/fa";
+import PropertyMap from "./PropertyMap";
 export default function PropertyDetails({ property }) {
   return (
     <main>
@@ -90,7 +92,7 @@ export default function PropertyDetails({ property }) {
       </div>
       {/* <!-- Map --> */}
       <div className="bg-white p-6 rounded-lg shadow-md mt-6">
-        <div id="map"></div>
+        <PropertyMap property={property} />
       </div>
     </main>
   );
