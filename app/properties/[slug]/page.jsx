e0239a9f7001dab59768b1502e588b0769f9471e
Anezz12 <harsenaargrtya1@gmail.com>
2024-08-30 18:29:16 +0700

    feat: Add BookmarkButton, PropertyContactForm, and ShareButton components

--- app/properties/[slug]/page.jsx
+++ app/properties/[slug]/page.jsx
@@ -1,11 +1,14 @@
 import PropertyHeaderImage from "@/components/PropertyHeaderImage";
-import connectDB from "@/config/database";
+import BookmarkButton from "@/components/BookmarkButton";
+import PropertyContactForm from "@/components/PropertyContactForm";
+import ShareButton from "@/components/ShareButton";
 import Property from "@/models/Property";
 import Link from "next/link";
 import { FaArrowLeft } from "react-icons/fa";
 import Propertyimages from "@/components/Propertyimages";
 import PropertyDetails from "@/components/PropertyDetails";
 import { convertToSerializedObject } from "@/utils/convertToObject";
+import connectDB from "@/config/database";
 export default async function PropertyPage({ params }) {
   await connectDB();
   const propertyDoc = await Property.findById(params.slug).lean();
@@ -38,6 +41,11 @@ export default async function PropertyPage({ params }) {
           <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
             {/* Property Info */}
             <PropertyDetails property={property} />
+            <aside className="space-y-4">
+              <BookmarkButton property={property} />
+              <ShareButton property={property} />
+              <PropertyContactForm property={property} />
+            </aside>
           </div>
         </div>
       </section>
