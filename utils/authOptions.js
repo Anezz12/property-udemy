efb9530ff38099f738174ed31044d752636a99b5
Anezz12 <harsenaargrtya1@gmail.com>
2024-10-19 12:25:20 +0700

    refactor: Refactor signIn and session callbacks in authOptions.js
    Refactor the signIn and session callbacks in the authOptions.js file to improve code readability and error handling. The signIn callback now connects to the database, checks if the user exists, and creates a new user if necessary. The session callback retrieves the user from the database and assigns the user ID to the session. Error handling has been added to both callbacks to log any errors encountered.

--- utils/authOptions.js
+++ utils/authOptions.js
@@ -1,6 +1,7 @@
 import GoogleProvider from "next-auth/providers/google";
 import connectDB from "@/config/database";
 import User from "@/models/User";
+
 export const authOptions = {
   providers: [
     GoogleProvider({
@@ -16,32 +17,36 @@ export const authOptions = {
     }),
   ],
   callbacks: {
-    // Invoked on successful sign in
     async signIn({ profile }) {
-      // connect to database
-      await connectDB();
-      // check if user exists
-      const userExists = await User.findOne({ email: profile.email });
-      if (!userExists) {
-        //Truncate user name if too long
-        const username = profile.name.slice(0, 20);
-        // create user in database
-        await User.create({
-          email: profile.email,
-          username,
-          image: profile.picture,
-        });
+      try {
+        await connectDB();
+        const userExists = await User.findOne({ email: profile.email });
+        if (!userExists) {
+          const username = profile.name.slice(0, 20);
+          await User.create({
+            email: profile.email,
+            username,
+            image: profile.picture,
+          });
+        }
+        return true;
+      } catch (error) {
+        console.error("Error in signIn callback:", error);
+        return false;
       }
-      // return true to sign in user
-      return true;
     },
     async session({ session }) {
-      // 1. GET User from database
-      const user = await User.findOne({ email: session.user.email });
-      // 2. Assign user to session
-      session.user.id = user._id.toString();
-      // 3. Return session
-      return session;
+      try {
+        await connectDB();
+        const user = await User.findOne({ email: session.user.email });
+        if (user) {
+          session.user.id = user._id.toString();
+        }
+        return session;
+      } catch (error) {
+        console.error("Error in session callback:", error);
+        return session;
+      }
     },
   },
 };
